fa6bb96c75e71bceafb72b66d0a9c91c111f25fd
max-drexler <mndrexler@gmail.com>
2023-10-02 15:44:11 -0500

    testing

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 $(window).on('load', function () {
     $("#loader-wrapper").fadeOut(700);
+
+    let ocTexts = ['Leather Worker', 'Test', 'Test2'];
+    let ocIndex = 0;
+
+    function rotateOcupations() {
+        $(".oc-text").fadeOut(500, function(){
+            $(this).text(ocTexts[ocIndex]).fadeIn(500);
+        });
+    
+        ocIndex = (ocIndex + 1) % ocTexts.length;
+    }
+
+    setInterval(rotateOcupations, 2000);
 });
