6f4815320329cc6e4d7302a9152d68186ffec823
max-drexler <mndrexler@gmail.com>
2023-12-31 19:43:11 -0600

    dynamically load occupations

--- script.js
+++ script.js
@@ -17,29 +17,42 @@ function project_factory(project_data){
     </div>`
 }
 
+function title_factory(index, title_name){
+    if(index == 0){
+        return `<h2 class="oc-text oc-text-shown">${title_name}</h2>`
+    }
+    return `<h2 class="oc-text">${title_name}</h2>`
+}
+
 $(window).on('load', function () {
-    $("#loader-wrapper").fadeOut(700);
-
-    // Occupation rotation
-    let occupations_len = $("#oc-container").children().length;
-    setInterval(() => {
-        $("#oc-container").children().eq(occupation_index).fadeOut({
-            duration: occupation_animation_length,
-            complete: () => {
-                occupation_index = (occupation_index + 1) % occupations_len;
-                $("#oc-container").children().eq(occupation_index).fadeIn({
-                    duration: occupation_animation_length,
-                });
-            }
-        }
-        );
-    }, occupation_rotate_length);
-
     // Project content
-    $.getJSON("projects.json", (data) => {
-        $.each(data, (key, val) => {
-            console.log(project_factory(val));
+    $.getJSON("settings.json", (data) => {
+        $.each(data["titles"], (key, val) => {
+            console.log(title_factory(val));
+            $("#oc-container").append(title_factory(key, val));
+        });
+        $.each(data["projects"], (key, val) => {
             $("main").append(project_factory(val));
         });
+        $("#oc-container:first-child").addClass("oc-text-shown");
+        let occupations_len = $("#oc-container").children().length;
+        setInterval(() => {
+            $("#oc-container").children().eq(occupation_index).fadeOut({
+                duration: occupation_animation_length,
+                complete: () => {
+                    occupation_index = (occupation_index + 1) % occupations_len;
+                    $("#oc-container").children().eq(occupation_index).fadeIn({
+                        duration: occupation_animation_length,
+                    });
+                }
+            }
+            );
+        }, occupation_rotate_length);
     });
+    $("#loader-wrapper").fadeOut(700);
+
+
+    // Occupation rotation
+    console.log($("#oc-container").children())
+
 });
